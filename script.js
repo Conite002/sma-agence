988c47b20d2c7dfa6e7d4bc82f125379c50f8f6c
conite002 <conite.gbodogbe@imsp-uac.org>
2022-09-06 01:35:15 +0200

    sma is ok

---
+++ script.js
@@ -0,0 +1,6 @@
+var bgImage = document.querySelector(".section-nav");
+
+// setInterval(()=>{
+// 	bgImage.style.transform = "scale(1.1)";
+// 	bgImage.style.transition = "all 8s ease";
+// },9000);
